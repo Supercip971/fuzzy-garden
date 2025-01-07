12921896fdbd866765e041b75fcf16dfa5589b37
cyp <supercyp971@gmail.com>
2025-01-07 16:44:14 +0100

    feat: update ds graph

--- ds_graph.py
+++ ds_graph.py
@@ -1,85 +1,111 @@
+###############
+### Fonctions
 
-class GraphLink: 
-    lhs: str 
-    rhs: str
-    weight: int
+def valeur_exploree(stack, x):
+    for v in stack:
+        for i in v:
+            if i == x:
+                return True 
 
-    def __init__(self, lhs, rhs, weight):
-        self.lhs = str(lhs)
-        self.rhs = str(rhs)
-        self.weight = weight
+    return False
 
-    def __str__(self):
-        return f"({self.lhs} <-> {self.rhs}, {self.weight})"
-    
-graph = [
-    GraphLink(6,4,30),
-    GraphLink(6,0,20),
-    GraphLink(5,0,50),
-    GraphLink(5,4,40),
-    GraphLink(4,0,20),
-    GraphLink(4,1,50),
-    GraphLink(4,3,40),
-    GraphLink(4,2,50),
-    GraphLink(3,2,20),
-    GraphLink(3,0,50),
-    GraphLink(2,0,40),
-    GraphLink(2,1,30),
-    GraphLink(1,0,30),
-]
+def affiche_largeur(s_init, adj) :
+    '''
+    s_init : sommet initial
+    adj : dico associant la liste des suivants à tout sommet ayant au moins un suivant
+    type des sommets quelconques pourvu qu'il y ait cohérence entre éléments de la liste et du dico.
+    ne retourne rien, fait juste un affichage
+    '''
+    levels = [] 
+    levels.append([s_init])
 
-def get_nodes(graph: list[GraphLink]) -> list[str]:
-    nodes = []
-    for link in graph: 
-        if link.lhs  not in nodes:
-            nodes.append(link.lhs)
-        if link.rhs  not in nodes:
-            nodes.append(link.rhs)
-    return nodes 
-
-
-
-# graph is in the form: 
-# { key: {path }
-
-# starts = R
-# end = E
-
-def choose_min_path(graph: list[GraphLink], starts: list[str]) -> str:
-    min_path = None
-    min_weight = None
-
-    for link in graph:
-        if min_weight is None or link.weight < min_weight:
-            if link.lhs in starts and not link.rhs in starts:
-                min_weight = link.weight
-                min_path = link
-            elif link.rhs in starts and not link.lhs in starts:
-                min_weight = link.weight
-                min_path = link
-
-    return min_path
-
-def min_couv(graph: list[GraphLink], initial_point: str):
-    r: list[str] = [initial_point]
-    e: list[GraphLink] = graph.copy()
-    cmg: list[GraphLink] = []
-    
-    lnub = len(get_nodes(graph))
-    while len(r) < lnub:
-        min_path = choose_min_path(e, r)
-        if(min_path is None):
-            break
-        cmg.append(min_path)
+    a_explorer = [s_init] 
+    while len(a_explorer) > 0:
+        niveau = []
+        for v in a_explorer:
+            
+            if not v in adj.keys():
+                continue
         
-        if min_path.lhs not in r:
-            r.append(min_path.lhs)
-        if min_path.rhs not in r:
-            r.append(min_path.rhs)
+            enfants = adj[v]
+            for e in enfants: 
+                if not valeur_exploree(levels, e) and not e in niveau:
+                    niveau.append(e)
         
-        print(min_path)
-        e.remove(min_path)
+        if len(niveau) != 0:
+            levels.append(niveau)
+        
+        a_explorer.clear()
+        for v in niveau:
+            a_explorer.append(v)
+    print(levels)
+    return levels
     
-    return cmg 
 
-print(min_couv(graph, "0"))
+
+def bfs(adj, s_init):
+    q = [s_init]
+    parent = {}
+    visited = [s_init]
+
+    while len(q) > 0:
+        u = q.pop(0)
+        if u in adj: 
+            for v in adj[u]:
+                if not v in visited:
+                    visited.append(v)
+                    parent[v] = u 
+                    q.append(v) 
+
+    return parent 
+
+def chemin(adj, root, v):
+    l = bfs(adj, root)
+
+    if root == v: 
+        return [root] 
+    elif not v in l.keys(): 
+        return []  
+    else: 
+        p = chemin(adj, root, l[v])
+        return p + [v]
+
+
+
+
+ 
+###############
+### Code principal
+
+## Définition d'un graphe
+## Représentation choisie : 
+## - une liste de sommets et 
+## - un dictionnaire associant à chaque sommet la liste des sommets adjacents (quand il y a des sommets)
+
+
+sommets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+adj = {
+'a' : [ 'b', 'c' ], #choix de l'ordre alphabétique pour les sommets adjacents d'un sommet
+'b' : [ 'a', 'e', 'h' ],
+'c' : [ 'd', 'e' ],
+'d':  [ 'e' ],
+'e' : [ 'f', 'g'],
+'f' :  [ 'g' ],
+'h' : ['i'],
+}
+
+
+
+## affichage des noeuds dans l'ordre du parcours en largeur à partir de a
+#print("Parcours en largeur à partir de 'a'")
+#affiche_largeur('a', adj)
+
+#print()
+
+## affichage des noeuds dans l'ordre du parcours en largeur à partir de c
+#print("Parcours en largeur à partir de 'c'")
+#affiche_largeur('c', adj)
+
+
+print(chemin(adj, 'c', 'f'))
