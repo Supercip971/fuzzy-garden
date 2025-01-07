5335161c98df9e1db874ed811eeacd33e3a913b5
lylykin <clelie.jalbert@gmail.com>
2025-01-07 17:05:01 +0100

    removed useless functions, created list-to-dico function

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-# hello worldo 
+import csv
+import ds_graph as g
 
-print("Hello World!")
+#récupérer les data du fichier data arcs
+with open("data/data_arcs.csv") as f_arcs:
+    r_arcs = csv.reader(f_arcs)
+    arcs = [row for row in r_arcs]  #liste de liste avec les arcs
+    
+def list_to_dico(liste):
+    dico = {}
+    for elt in liste:
+        if elt[1] == 'favorise':
+            dico[elt[0]] = dico.get(elt[0],[]) + [elt[2]]
+
+    
