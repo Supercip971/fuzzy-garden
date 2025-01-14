d559f0b6ec174390397451cbb95f85e7be6479b6
lylykin <clelie.jalbert@gmail.com>
2025-01-14 18:03:01 +0100

    improving dijkstra subfunctions

--- ds_pgraph.py
+++ ds_pgraph.py
@@ -3,20 +3,33 @@ import math
 def d_init(o_graph, s):
     """ initialiser les distances"""
     dist = {}
+    
     for som in o_graph.keys():
         dist[som] = math.inf
     
     dist[s] = 0
+    return dist
 
-def isCouvrant(g1, g2): 
-    """ regarde si g1 est un graphe couvrant de g2 (tous ses noeuds sont dedans)"""
-    for s in g1.keys(): 
-        if s not in g2.keys():
-            return False
-        
-    return True
 
-def dijkstra(o_graph, s_deb):
-    d_graph = {}
-    dist_graph = d_init(o_graph, s_deb)
+def find_min(n_list, dico_dist):
+    mini = math.inf
     
+    for s in n_list : 
+        
+        if  dico_dist[s] < mini : 
+            mini = dico_dist[s]
+            s_min = s
+        
+    return s_min
+    
+
+def Dijkstra(o_graph, s_deb):
+    d_graph = {}
+    dico_dist = d_init(o_graph, s_deb) #init de la distance
+    to_visit = [s for s in o_graph.keys()]
+    
+    while to_visit != []:     #tant que tous les sommets de o_graph ne sont pas dans d_graph
+        s = find_min(to_visit, dico_dist)
+        to_visit.remove(s)
+        
+        
