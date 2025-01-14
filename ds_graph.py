cdaa55555d72409374122d7b3ce345808fc0ad26
cyp <supercyp971@gmail.com>
2025-01-14 15:45:11 +0100

    style: tyding up code

--- ds_graph.py
+++ ds_graph.py
@@ -25,29 +25,3 @@ def chemin(adj, root, v):
         p = chemin(adj, root, l[v])
         return p + [v]
 
-
-
-
- 
-###############
-### Code principal
-
-## Définition d'un graphe
-## Représentation choisie : 
-## - une liste de sommets et 
-## - un dictionnaire associant à chaque sommet la liste des sommets adjacents (quand il y a des sommets)
-
-
-sommets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
-
-adj = {
-'a' : [ 'b', 'c' ], #choix de l'ordre alphabétique pour les sommets adjacents d'un sommet
-'b' : [ 'a', 'e', 'h' ],
-'c' : [ 'd', 'e' ],
-'d':  [ 'e' ],
-'e' : [ 'f', 'g'],
-'f' :  [ 'g' ],
-'h' : ['i'],
-}
-
-
--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import csv
 import ds_graph as g
 
+
 def list_to_dico(liste):
-    """Entrée : liste (list)
-    ---
-    convertit une liste de listes de taille 3 en dico
-    ---
-    Sortie : dico (dict)"""
+    """
+    Convertit une liste de listes de taille 3 en dico
+    
+    Paramètres
+    ----------
+    liste : list
+        Liste à convertire au format de liste de liste, importée du fichier data_arcs.csv
+    
+    Retourne
+    -------
+    dico : dict
+        Dictionnaire qui associe chaque entrée au destinations favorisées possibles
+    """
     
     dico = {}
     for elt in liste:
         if elt[1] == 'favorise':
+            # FIXME: A réécrire c'est degueu ça 
+            # on dois réfléchir pour comprendre ce que ça fait
+            # <?> un bon code ne dois pas bloquer à la compréhension d'un algo  
             dico[elt[0]] = dico.get(elt[0],[]) + [elt[2]]
 
     return(dico)
 
-#récupérer les data du fichier data arcs
-with open("data/data_arcs.csv") as f_arcs:
+
+list_to_dico()
+
+# Récupérer les data du fichier data arcs
+with open("data/data_arcs.csv", encoding="utf-8") as f_arcs:
     r_arcs = csv.reader(f_arcs, delimiter=";")
     arcs = [row for row in r_arcs]  #liste de liste avec les arcs
- 
+
  
 A=str(input("premier indrédient : ")) 
 B=str(input('2e ingrédient : '))  
 print(g.chemin(list_to_dico(arcs), A, B))
-    
