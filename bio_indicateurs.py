86df8bc9a73bbdca3b2f7ebb6608dd91d0a68933
cyp <supercyp971@gmail.com>
2025-01-14 17:47:00 +0100

    feat: add bio indicateurs

---
+++ bio_indicateurs.py
@@ -0,0 +1,60 @@
+
+import csv
+
+bio_indicateurs_data = []
+
+def bio_indicateur_espece(espece):
+    """
+    Retourne la liste des bio-indicateurs de l'espèce passée en paramètre.
+    
+    Paramètres
+    ----------
+    espece : str
+        Nom de l'espèce dont on veut les bio-indicateurs.
+    
+    Retourne
+    -------
+    bio_indicateurs : list
+        Liste des bio-indicateurs de l'espèce passée en paramètre.
+    """
+
+    for i in bio_indicateurs_data:
+        if i[0] == espece:
+            return i[1:]
+    return None
+
+def bio_indicateurs_init(): 
+    """
+    Charge les bio-indicateurs depuis le fichier 'data_arcs_poids.csv' dans la 
+    variable globale bio_indicateurs_data.
+    """
+    data = []    
+
+    with open("data/data_arcs_poids.csv", encoding="utf-8") as f_bioind:
+        r_bioind = csv.reader(f_bioind, delimiter=";")
+        data = [row for row in r_bioind]  #liste de liste avec les arcs
+
+    return data
+
+# Plus le score est bas 
+def bio_indicateurs_malus(el1, el2):
+    esp1 = bio_indicateur_espece(el1)
+    esp2 = bio_indicateur_espece(el2)
+    
+    # si l'une des deux espèces n'est pas listée, nous ne considérons pas de malus 
+    if esp1 == None or esp2 == None: 
+        return 0
+
+    # si les deux espèces sont listées, nous calculons le malus par la distance 
+    # entre les deux espèces
+    # pour chaque bio indicateur, on calcule la distance 
+    #   dist = somme |esp1_i - esp2_i|
+
+    dist = 0
+    for i in range(len(esp1)):
+        dist += abs(int(esp1[i]) - int(esp2[i]))
+    
+    return dist
+    
+    
+bio_indicateurs_data = bio_indicateurs_init()
